770bebbf57fa32483a1607fd041422eda485d5a2
Kasem Chooratna <Kasem.ch@rumail.ru.ac.th>
2026-09-13 23:49:49 +0700

    Add HED3505 live smoke test configuration

---
+++ hed3505-interactive-lab/live-config.js
@@ -0,0 +1,5 @@
+window.HED3505_LIVE_CONFIG = {
+  projectUrl: "https://lztxpjsuzqvtgyasfnyj.supabase.co",
+  publishableKey: "sb_publishable_7bv5GR0-ksXJn91sRHV0Mg_k4nblIGI",
+  pilotActivitySessionId: "408bdd66-7c77-4faf-83e2-12c8cceaa146"
+};
